7d377b5842ad26d6b1cf3300b88d22d145b13094
rfdias <reinaldo@rfdias-2.local>
2024-04-01 20:09:24 -0300

    get from DB lambda

---
+++ app/get_from_db.js
@@ -0,0 +1,34 @@
+// Load the AWS SDK for Node.js
+const AWS = require("aws-sdk");
+// Create DynamoDB document client
+let dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
+
+
+// Define handler function, the entry point to our code for the Lambda service
+// We receive the object that triggers the function as a parameter
+exports.handler = function(event, context, callback){
+   
+    // Create JSON object with parameters for DynamoDB and store in a variable
+//	let id = event.inputID;
+    let params = {
+        TableName:'HelloWorldDatabase',
+        Key: {
+           "ID":  "2"
+        }
+    };
+    // Using await, make sure object writes to DynamoDB table before continuing execution
+    dynamodb.get(params, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+        
+      }
+       
+    callback(null, JSON.stringify(data));
+    });
+    
+   
+    
+}
+;
