968d67a33f34b03696610309059332d5cae3a294
rfdias <reinaldo@rfdias-2.local>
2024-04-14 19:13:38 -0300

    lambda deploy

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -10,18 +10,19 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "HelloWorldFunction" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "../app/lambda1.js"
+  filename      = "../app/lambda1.zip"
   function_name = "HelloWorldFunction"
   role          = "arn:aws:iam::784488449591:role/service-role/HelloWorldFunction-role-whmhi9c5"
   handler       = "index.handler"
 
-  source_code_hash = "g5wmtk3jCMfUcrXvhucwGkuHMWzk7htwWX2CJQMmMZw="
+  //source_code_hash = base64sha256(file("../app/lambda1.js"))
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "nodejs16.x"
 
   tags = {
     Env = "DEV"
-    Managedby = "Terraform"
+    "Managed by" = "Terraform"
   }
 
   
