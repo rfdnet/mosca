873f4e95b50e2c13268c3a26d30d1fe3f7854c7b
rfdias <reinaldo@rfdias-2.local>
2024-05-12 19:05:18 -0300

    fix lambda zip package

--- app/lambda/lambda-zip.sh
+++ app/lambda/lambda-zip.sh
@@ -1,3 +1,5 @@
 rm */*.zip
 
-zip insertDB/lambda.zip insertDB/index.js ; zip get_from_db/lambda.zip lambda.zip get_from_db/index.js ; zip helloworld/lambda.zip helloworld/index.js
+zip -j insertDB/lambda.zip insertDB/index.js
+zip -j get_from_db/lambda.zip lambda.zip get_from_db/index.js 
+zip -j helloworld/lambda.zip helloworld/index.js
