c53edee3912a47e51122b136d2f2beba11b9a7c6
rfdias <reinaldo@rfdias-2.local>
2024-05-12 12:53:27 -0300

    terraform import

--- infra/lambda-functions.tf
+++ infra/lambda-functions.tf
@@ -37,6 +37,37 @@ resource "aws_lambda_function" "insertDB" {
 
 }
 
-resource "aws_lambda_function" "helloword" {
 
+resource "aws_lambda_function" "helloworld" {
+   # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "../app/lambda/helloworld/lambda.zip"
+  function_name = "helloworld"
+  role          = "arn:aws:iam::784488449591:role/service-role/insertDB-role-f24q2iu4"
+  handler       = "index.handler"
+
+  #//source_code_hash = base64sha256(file("../app/lambda1.js"))
+  source_code_hash = "IcPjz2vXr6Fw1R1xdtvqTPm8cdZN99ZEZYg8xn7B5FI="
+  runtime          = "nodejs16.x"
+  ephemeral_storage {
+    size = 512
+  }
+  architectures = ["x86_64"]
+
+  logging_config {
+    log_format       = "JSON"
+    log_group        = "/aws/lambda/helloworld"
+    system_log_level = "DEBUG"
+  }
+
+  memory_size = 128
+
+  package_type = "Zip"
+
+
+
+  tags = {
+    Env          = "dev"
+    "Managed by" = "Terraform"
+  }
 }
