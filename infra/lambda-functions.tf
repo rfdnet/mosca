07c13d5f2e50b4057b302036d72370ccc6fc6196
rfdias <reinaldo@rfdias-2.local>
2024-06-02 14:10:52 -0300

    new buttons for test

--- infra/lambda-functions.tf
+++ infra/lambda-functions.tf
@@ -64,4 +64,37 @@ resource "aws_lambda_function" "helloworld" {
    Env          = "dev"
    "Managed by" = "Terraform"
  }
+
+
+}
+// zip file for lambda_get
+data "archive_file" "lambda_get" {
+  type        = "zip"
+  source_file = "${path.module}/../app/lambda/get_from_db/index.js"
+  output_path = "get_from_db.zip"
+}
+
+// lambda get_from_db
+resource "aws_lambda_function" "lambda_get" {
+ filename      = "get_from_db.zip"
+ function_name = "lambda_get"
+ role          = "arn:aws:iam::784488449591:role/service-role/insertDB-role-f24q2iu4"
+ handler       = "index.handler"
+ source_code_hash = data.archive_file.lambda_get.output_base64sha256 
+ runtime          = "nodejs16.x"
+ ephemeral_storage {
+   size = 512
+ }
+ architectures = ["x86_64"]
+ logging_config {
+   log_format       = "JSON"
+   log_group        = "/aws/lambda/lambda_get"
+   system_log_level = "DEBUG"
+ }
+ memory_size = 128
+ package_type = "Zip"
+ tags = {
+   Env          = "dev"
+   "Managed by" = "Terraform"
+ }
 }
